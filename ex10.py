e83f80831b92253e6f76f675f97a48584e6ce3f0
clondon <charles@new-web-sites.com>
2020-01-18 22:43:19 +0000

    Testing with ex10 - 18th January 2020

---
+++ ex10.py
@@ -0,0 +1,12 @@
+# 18th January 2020
+print("Hello")
+print("Practice Escape")
+print("I am happy to be \"Alive\"")
+typeo = """
+It's getting late have you seen my mates
+Ma tell me when the boy's get here.
+
+It's Saturday night and I to wanna rock,
+I want to get a belly full of beer.
+"""
+print(typeo)
